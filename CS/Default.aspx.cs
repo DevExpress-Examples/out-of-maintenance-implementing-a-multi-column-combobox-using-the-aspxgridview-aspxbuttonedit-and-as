9a1da34872eb2274b675148f8a82cb32cd3585a7
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-16 12:42:10 +0300

    initial commit

---
+++ CS/Default.aspx.cs
@@ -0,0 +1,15 @@
+﻿using DevExpress.Web.ASPxGridView;
+using System;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected virtual void Page_Load(object sender, EventArgs e)
+    {
+        ASPxGridView1.Width = ASPxButtonEdit1.Width;
+    }
+    protected void ASPxGridView1_HtmlRowCreated(object sender, ASPxGridViewTableRowEventArgs e)
+    {
+        if (e.RowType == GridViewRowType.Data)
+            e.Row.Attributes["data"] = e.GetValue("CategoryName").ToString();
+    }
+}
---
+++ VB/Default.aspx.vb
@@ -0,0 +1,15 @@
+﻿Imports DevExpress.Web.ASPxGridView
+Imports System
+
+Partial Public Class _Default
+    Inherits System.Web.UI.Page
+
+    Protected Overridable Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+        ASPxGridView1.Width = ASPxButtonEdit1.Width
+    End Sub
+    Protected Sub ASPxGridView1_HtmlRowCreated(ByVal sender As Object, ByVal e As ASPxGridViewTableRowEventArgs)
+        If e.RowType = GridViewRowType.Data Then
+            e.Row.Attributes("data") = e.GetValue("CategoryName").ToString()
+        End If
+    End Sub
+End Class
